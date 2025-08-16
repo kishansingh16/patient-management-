03d484af350f4cdb97628334993e6cfe9f786e35
Kishan Singh <92136795+kishansingh16@users.noreply.github.com>
2025-08-16 18:29:39 +0530

    "commit"

--- src/main/java/com/pm/patientservice/controller/PatientController.java
+++ src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -2,7 +2,7 @@ package com.pm.patientservice.controller;
 
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.service.PatientService;
-import org.springframework.http.ResponseEntity;   // ✅ import this
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
